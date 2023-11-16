50d795877340e3d1cbcb9d7f2c2bc93223285925
TomasAlbert <tomasalbert9387182@gmail.com>
2023-11-16 22:10:59 +0100

    add hamburger with global context

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import NavbarContext from "./context/NavbarContext";
+import { useState } from "react";
 
 const App = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<>
-			<Header />
-			<HomePage />
-			<ToastContainer position="top-rigth" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
+			<NavbarContext.Provider value={{ isOpen, setIsOpen }}>
+				<Header />
+				<HomePage />
+				<ToastContainer position="top-right" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
+			</NavbarContext.Provider>
 		</>
 	);
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
+import { useContext } from "react";
+import NavbarContext from "../context/NavbarContext.js";
+
 const Header = () => {
+	const { isOpen, setIsOpen } = useContext(NavbarContext);
+	const handleClick = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<header>
 			<nav>
 				<h1>🌏 Travel list.</h1>
+				<button onClick={handleClick} className={`${!isOpen ? "hamburger" : "hamburger active"}`}>
+					<div className="line"></div>
+					<div className="line"></div>
+					<div className="line"></div>
+				</button>
 			</nav>
 		</header>
 	);

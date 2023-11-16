9bd5b61721d672dc2013b2991deaf706662ab933
TomasAlbert <tomasalbert9387182@gmail.com>
2023-11-16 22:58:52 +0100

    add mobile menu

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,13 @@
 import SidebarHeading from "./SidebarHeading";
 import TravelCardList from "./TravelCardList";
+import { useContext } from "react";
+import NavbarContext from "../context/NavbarContext.js";
 
 const SideBar = ({ pins, isLoading, onItemdPinDelete, onCardClick }) => {
+	const { isOpen } = useContext(NavbarContext);
+
 	return (
-		<aside className="sidebar">
+		<aside className={!isOpen ? "sidebar" : "sidebar active"}>
 			<SidebarHeading heading={"Tvoj travel list."} />
 			<TravelCardList pins={pins} isLoading={isLoading} onItemdPinDelete={onItemdPinDelete} onCardClick={onCardClick} />
 		</aside>
